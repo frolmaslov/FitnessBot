a46ed93ad2802adabf02d5defb52140a90483eb2
frolmaslov <frol_mv@mail.ru>
2023-01-14 10:52:44 +0100

    comm

--- database_pgs.py
+++ database_pgs.py
@@ -1,10 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import *
+from config import SQLALCHEMY_DATABASE_URL
 
-
-SQLALCHEMY_DATABASE_URL = f'postgresql://{user}:{password}@localhost:5432/{db_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from sqlalchemy import Column, Integer, String, DateTime, Boolean
-from database import date
+import datetime
 from database_pgs import Base
 
+date = str(datetime.datetime.now().strftime("%Y-%m-%d"))
 
 class User(Base):
     __tablename__ = "users"
@@ -12,7 +13,7 @@ class User(Base):
     weight = Column(Integer)
     weight_wished = Column(Integer)
     online = Column(Boolean, default=True)
-    date = Column(String)
+    date = Column(DateTime)
 
     def __repr__(self):
         return f"<User {self.name} telegram_id={self.telegram_id} weight={self.weight} weight_wished={self.weight_wished} online={self.online} date={self.date}>"
